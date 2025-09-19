4f8cb8839c441639d08820be41ba06775884f7c7
davy.andrade <85322447+davyswtz@users.noreply.github.com>
2025-09-19 00:14:03 -0300

    feat: card criado

--- chute-premiado/src/app/features/home/home.ts
+++ chute-premiado/src/app/features/home/home.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
+import { NgFor, DatePipe } from '@angular/common';
+
+type Jogo = {
+  casaNome: string;
+  casaLogo: string;
+  foraNome: string;
+  foraLogo: string;
+  data: Date;
+};
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  templateUrl: './home.html',   // usa o SEU arquivo existente
-  styleUrl: './home.scss'       // pode criar vazio se não tiver
+  templateUrl: './home.html',
+  styleUrls: ['./home.scss'],
+  imports: [NgFor, DatePipe]
 })
-export class HomeComponent {}
+export class HomeComponent {
+  jogos: Jogo[] = [
+    {
+      casaNome: 'Cruzeiro',
+      casaLogo: 'assets/img/cruzeiro.png',
+      foraNome: 'Atlético-MG',
+      foraLogo: 'assets/img/atletico-mg.png',
+      data: new Date('2025-09-18T21:30:00')
+    },
+    {
+      casaNome: 'Palmeiras',
+      casaLogo: 'assets/img/palmeiras.png',
+      foraNome: 'São Paulo',
+      foraLogo: 'assets/img/saopaulo.png',
+      data: new Date('2025-09-20T16:00:00')
+    }
+  ];
+}

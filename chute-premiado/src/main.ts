f828481313898e66849bdb46ba1a306944a33207
davy.andrade <85322447+davyswtz@users.noreply.github.com>
2025-09-19 00:01:59 -0300

    feat: banner e background criado

--- chute-premiado/src/main.ts
+++ chute-premiado/src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // <-- adiciona isso
+
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
